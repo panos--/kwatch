f9a1035d31ee697442dc50cda76d8b6b0b6082db
Thomas Weiß <panos@unbunt.org>
2019-09-13 20:45:14 +0200

    WIP: Exec by dropping out of blessed instead of using blessed terminal

--- src/lib/actions/exec_action.ts
+++ src/lib/actions/exec_action.ts
@@ -13,40 +13,61 @@ export class ExecAction implements Action {
     }
 
     public execute(client: K8sClient, screen: blessed.Widgets.Screen, namespace: V1Namespace, apiResource: APIResource, resource: string) {
-        const term = blessed.terminal({
-            parent: screen,
-            label: "Terminal",
-            shell: "kubectl",
-            // TODO: specify container
-            // args: ["-n", namespace, "exec", "-it", resource, "-c", containerName, "--", "bash"],
-            args: ["-n", namespace.metadata.name, "exec", "-it", resource, "--", "bash"],
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            mouse: true,
-            keys: true,
-            border: "line",
-            scrollbar: {
-                ch: " ",
-                track: {
-                    bg: "cyan"
-                },
-                style: {
-                    inverse: true
-                }
-            },
-        });
-        
-        term.on("exit", () => {
-            term.write("Finished. Press ENTER to close.");
-            term.key("enter", () => {
-                term.kill();
-                term.destroy();
-            });
-        });
+        // const term = blessed.terminal({
+        //     parent: screen,
+        //     label: "Terminal",
+        //     shell: "kubectl",
+        //     // TODO: specify container
+        //     // args: ["-n", namespace, "exec", "-it", resource, "-c", containerName, "--", "bash"],
+        //     args: ["-n", namespace.metadata.name, "exec", "-it", resource, "--", "bash"],
+        //     top: 0,
+        //     left: 0,
+        //     width: "100%",
+        //     height: "100%",
+        //     mouse: true,
+        //     keys: true,
+        //     border: "line",
+        //     scrollbar: {
+        //         ch: " ",
+        //         track: {
+        //             bg: "cyan"
+        //         },
+        //         style: {
+        //             inverse: true
+        //         }
+        //     },
+        // });
 
-        term.focus();
-        screen.render();
+        // term.on("exit", () => {
+        //     term.write("Finished. Press ENTER to close.");
+        //     term.key("enter", () => {
+        //         term.kill();
+        //         term.destroy();
+        //     });
+        // });
+
+        // term.focus();
+        // screen.render();
+
+        let width: number = typeof screen.width == "number" ? screen.width : parseInt(screen.width);
+        let height: number = typeof screen.height == "number" ? screen.height : parseInt(screen.height);
+        screen.clearRegion(0, width, 0, height);
+        screen.cursorReset();
+        // screen.render();
+        screen.program.showCursor();
+        screen.program.restoreCursor();
+        screen.exec("kubectl", [
+            "-n",
+            namespace.metadata.name,
+            "exec",
+            "-it",
+            resource,
+            "--",
+            "bash"
+        ], {}, (err, success) => {
+            if (err) {
+                console.log(err);
+            }
+        });
     }
 }

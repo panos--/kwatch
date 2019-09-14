eea34573a48211c981694841865ea75520714da2
Thomas Weiß <panos@unbunt.org>
2019-09-14 13:31:44 +0200

    Refactor exec code

--- src/lib/actions/exec_action.ts
+++ src/lib/actions/exec_action.ts
@@ -3,6 +3,7 @@ import { Action } from "./action";
 import { V1Namespace } from "@kubernetes/client-node";
 import { APIResource, K8sClient } from "../client";
 import { AppDefaults } from "../app_defaults";
+import { BlessedUtils } from "../blessed_utils";
 
 export abstract class ExecAction implements Action {
     public abstract getLabel(): string;
@@ -83,55 +84,6 @@ export abstract class ExecAction implements Action {
     }
 
     private executeCommand(screen: blessed.Widgets.Screen, namespace: V1Namespace, resource: string, container: string) {
-        // NOTE: Run command in a terminal box inside blessed's screen. Left here just for reference.
-
-        // const term = blessed.terminal({
-        //     parent: screen,
-        //     label: "Terminal",
-        //     shell: "kubectl",
-        //     // TODO: specify container
-        //     // args: ["-n", namespace, "exec", "-it", resource, "-c", containerName, "--", "bash"],
-        //     args: ["-n", namespace.metadata.name, "exec", "-it", resource, "--", "bash"],
-        //     top: 0,
-        //     left: 0,
-        //     width: "100%",
-        //     height: "100%",
-        //     mouse: true,
-        //     keys: true,
-        //     border: "line",
-        //     scrollbar: {
-        //         ch: " ",
-        //         track: {
-        //             bg: "cyan"
-        //         },
-        //         style: {
-        //             inverse: true
-        //         }
-        //     },
-        // });
-
-        // term.on("exit", () => {
-        //     term.write("Finished. Press ENTER to close.");
-        //     term.key("enter", () => {
-        //         term.kill();
-        //         term.destroy();
-        //     });
-        // });
-
-        // term.focus();
-        // screen.render();
-
-        // NOTE: although screen.spawn() does various things to clean up the screen before launching
-        // the given command this doesn't work somehow and leave the screen in cluttered state.
-        // Therefore, before calling spawn() we run a sequence of commands taken from blessed's
-        // exit routine.
-        let height = typeof screen.height == "number" ? screen.height : parseInt(screen.height);
-        screen.program.csr(0, height - 1);
-        screen.program.showCursor();
-        screen.alloc();
-        screen.program.normalBuffer();
-        screen.program.flush();
-
         const command = "kubectl";
         const args = [
             "-n",
@@ -145,16 +97,13 @@ export abstract class ExecAction implements Action {
         ];
         args.push.apply(args, this.getCommand());
 
-        screen.log(`executing command: ${command} ${args.join(" ")}`);
-
-        screen.exec(command, args, {}, (err, success) => {
+        BlessedUtils.executeCommand(screen, command, args, (err, success) => {
             if (err) {
                 console.log(err);
             }
             if (!success) {
                 console.log("Command returned with non-zero exit code");
             }
-            screen.program.hideCursor();
         });
     }
 }
---
+++ src/lib/blessed_utils.ts
@@ -0,0 +1,62 @@
+import * as blessed from "blessed";
+
+export class BlessedUtils {
+    public static executeCommand(screen: blessed.Widgets.Screen, command: string, args: string[],
+        callback: (err: any, succes: boolean) => void) {
+        // NOTE: Run command in a terminal box inside blessed's screen. Left here just for reference.
+
+        // const term = blessed.terminal({
+        //     parent: screen,
+        //     label: "Terminal",
+        //     shell: "kubectl",
+        //     // TODO: specify container
+        //     // args: ["-n", namespace, "exec", "-it", resource, "-c", containerName, "--", "bash"],
+        //     args: ["-n", namespace.metadata.name, "exec", "-it", resource, "--", "bash"],
+        //     top: 0,
+        //     left: 0,
+        //     width: "100%",
+        //     height: "100%",
+        //     mouse: true,
+        //     keys: true,
+        //     border: "line",
+        //     scrollbar: {
+        //         ch: " ",
+        //         track: {
+        //             bg: "cyan"
+        //         },
+        //         style: {
+        //             inverse: true
+        //         }
+        //     },
+        // });
+
+        // term.on("exit", () => {
+        //     term.write("Finished. Press ENTER to close.");
+        //     term.key("enter", () => {
+        //         term.kill();
+        //         term.destroy();
+        //     });
+        // });
+
+        // term.focus();
+        // screen.render();
+
+        // NOTE: although screen.spawn() does various things to clean up the screen before launching
+        // the given command this doesn't work somehow and leave the screen in cluttered state.
+        // Therefore, before calling spawn() we run a sequence of commands taken from blessed's
+        // exit routine.
+        let height = typeof screen.height == "number" ? screen.height : parseInt(screen.height);
+        screen.program.csr(0, height - 1);
+        screen.program.showCursor();
+        screen.alloc();
+        screen.program.normalBuffer();
+        screen.program.flush();
+
+        screen.log(`executing command: ${command} ${args.join(" ")}`);
+
+        screen.exec(command, args, {}, (err, success) => {
+            screen.program.hideCursor();
+            callback(err, success);
+        });
+    }
+}

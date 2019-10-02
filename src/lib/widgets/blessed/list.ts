ba43d2fcc183c26d8fc26713cd2bc0dfded21af3
Thomas Weiß <panos@unbunt.org>
2019-10-02 21:12:51 +0200

    fix(apilist): fix resource list not focused when selecting api resource

--- src/lib/widgets/blessed/list.ts
+++ src/lib/widgets/blessed/list.ts
@@ -9,29 +9,31 @@ export class List extends blessed.widget.List {
     public constructor(options: blessed.Widgets.ListOptions<blessed.Widgets.ListElementStyle>) {
         super(options);
 
-        this.key("home", () => {
-            this.select(0);
-            this.scrollTo(0);
-        });
-        this.key("end", () => {
-            const dstIndex = this.children.length - 1;
-            this.select(dstIndex);
-            this.scrollTo(dstIndex);
-        });
-        this.key("pageup", () => {
-            // NOTE: not correct but better than nothing (scroll jumps on refreshes...)
-            const offset = -(this.height / 2 | 0) || -1;
-            const dstIndex = this.getSelectedIndex() + offset;
-            this.select(dstIndex);
-            this.scrollTo(dstIndex);
-        });
-        this.key("pagedown", () => {
-            // NOTE: not correct but better than nothing (scroll jumps on refreshes...)
-            const offset = (this.height / 2 | 0) || -1;
-            const dstIndex = this.getSelectedIndex() + offset;
-            this.select(dstIndex);
-            this.scrollTo(dstIndex);
-        });
+        if (options.keyable === true || options.keys === true) {
+            this.key("home", () => {
+                this.select(0);
+                this.scrollTo(0);
+            });
+            this.key("end", () => {
+                const dstIndex = this.children.length - 1;
+                this.select(dstIndex);
+                this.scrollTo(dstIndex);
+            });
+            this.key("pageup", () => {
+                // NOTE: not correct but better than nothing (scroll jumps on refreshes...)
+                const offset = -(this.height / 2 | 0) || -1;
+                const dstIndex = this.getSelectedIndex() + offset;
+                this.select(dstIndex);
+                this.scrollTo(dstIndex);
+            });
+            this.key("pagedown", () => {
+                // NOTE: not correct but better than nothing (scroll jumps on refreshes...)
+                const offset = (this.height / 2 | 0) || -1;
+                const dstIndex = this.getSelectedIndex() + offset;
+                this.select(dstIndex);
+                this.scrollTo(dstIndex);
+            });
+        }
     }
 
     public getSelectedIndex() {
